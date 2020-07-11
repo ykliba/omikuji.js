91f07e23a6aa8423fecc66d5f3b7b23229b676dd
ykliba <mediaboxing2@gmail.com>
2020-07-11 10:36:08 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,17 @@
     // btn.textContent = results[Math.floor(Math.random() * results.length)];
     const n = Math.random();
     if (n < 0.05) {
-      btn.textContent = '大吉'; //5%
-    } else if (n < 0.2) {
-      btn.textContent = '中吉'; //15%
+      btn.textContent = '大吉'; 
+    } else if (n < 0.1) {
+      btn.textContent = '中吉'; 
+    } else if (n < 0.2){
+      btn.textContent = '小吉';
+    } else if (n < 0.3) {
+      btn.textContent = '吉';
+    } else if (n < 0.4) {
+      btn.textContent = '末吉';
     } else {
-      btn.textContent = '凶'; //80%
+      btn.textContent = '凶'; 
     }
     
 
